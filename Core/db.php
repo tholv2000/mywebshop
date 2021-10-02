16be5994b68235eeb1df521c48e390bf31cfe4d6
tholv2000 <45034846+tholv2000@users.noreply.github.com>
2021-10-02 19:03:30 +0700

    Update db.php

--- Core/db.php
+++ Core/db.php
@@ -1,7 +1,7 @@
 <?php 	
 	class db{
 		//ham lay tat ca cac ban ghi
-		public function get_all($sql, $arr = NULL){
+		public function get_all($sql){
 			//lay bien ket noi
 			$conn = connection::getInstance();
 			//chuan bi truy van
@@ -9,13 +9,13 @@
             //xac dinh kieu duyet phan tu
             $query->setFetchMode(PDO::FETCH_OBJ);
             //thuc thi truy van
-            $query->execute($arr);
+            $query->execute();
             //lay toan bo du lieu
             $result = $query->fetchAll();
             return $result;
 		}
 		//ham lay mot ban ghi
-		public function get_one($sql, $arr = NULL){
+		public function get_one($sql){
 			//lay bien ket noi
 			$conn = connection::getInstance();
 			//chuan bi truy van
@@ -23,13 +23,13 @@
             //xac dinh kieu duyet phan tu
             $query->setFetchMode(PDO::FETCH_OBJ);
             //thuc thi truy van
-            $query->execute($arr);
+            $query->execute();
             //lay toan bo du lieu
             $result = $query->fetch();
             return $result;
 		}
 		//ham dem so luong ban ghi
-		public function count($sql, $arr = NULL){
+		public function count($sql){
 			//lay bien ket noi
 			$conn = connection::getInstance();
 			//chuan bi truy van
@@ -37,7 +37,7 @@
             //xac dinh kieu duyet phan tu
             $query->setFetchMode(PDO::FETCH_OBJ);
             //thuc thi truy van
-            $query->execute($arr);
+            $query->execute();
             //lay toan bo du lieu
             $result = $query->rowCount();
             return $result;
