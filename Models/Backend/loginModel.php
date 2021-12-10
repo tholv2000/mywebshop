2642d36067b1ca6176df42615ad07f8fd1b5fa23
tholv2000 <45034846+tholv2000@users.noreply.github.com>
2021-12-10 12:13:08 +0700

    Update loginModel.php

--- Models/Backend/loginModel.php
+++ Models/Backend/loginModel.php
@@ -3,15 +3,15 @@
 	trait loginModel{
 		//ham lay mot ban ghi
 		public function find($email, $password){
+			$password = md5($password);
 			//lay bien ket noi de thao tac CSDL bang cach goi ham getInstance trong class Connection -> file core/connection.php
 			$db = connection::getInstance();
-			$password = md5($password);
 			$query = $db->prepare("select email, password from tbl_user where email=:email and password=:password");
 			$query->setFetchMode(PDO::FETCH_OBJ);
 			$query->execute(array("email"=>$email,"password"=>$password));
 			//lay mot phan tu
-			$result = $query->fetch();
-			if(isset($result->email)){
+			$result = $query->fetchAll();
+			if(sizeof($result) > 0){
 				
 					//dang nhap thanh cong
 					$_SESSION["email"] = $email;
