0dde0e19d1a3003ab1f3480fd1d9e1a42bf89837
tholv2000 <45034846+tholv2000@users.noreply.github.com>
2021-12-10 10:29:33 +0700

    Update loginModel.php

--- Models/Backend/loginModel.php
+++ Models/Backend/loginModel.php
@@ -5,20 +5,19 @@
 		public function find($email, $password){
 			//lay bien ket noi de thao tac CSDL bang cach goi ham getInstance trong class Connection -> file core/connection.php
 			$db = connection::getInstance();
-			$query = $db->prepare("select email, password from tbl_user where email=:email");
+			$password = md5($password);
+			$query = $db->prepare("select email, password from tbl_user where email='$email' and password='$password'");
 			$query->setFetchMode(PDO::FETCH_OBJ);
-			$query->execute(array("email"=>$email));
+			$query->execute();
 			//lay mot phan tu
 			$result = $query->fetch();
 			if(isset($result->email)){
-				if($result->password == md5($password)){
+				
 					//dang nhap thanh cong
 					$_SESSION["email"] = $email;
 					//di chuyen den duong dan
 					header("location:index.php?area=admin&controller=home&action=index");
-				}else
-					//di chuyen den duong dan
-					header("location:index.php?area=admin&controller=login&action=index");
+				
 			}else
 				//di chuyen den duong dan
 				header("location:index.php?area=admin&controller=login&action=index");
